4a769fc82c09f592df3a96bfce68cd6f303cf9fe
DrSilence <DrSilence@users.noreply.github.com>
2015-02-12 23:04:26 +0100

    added gettext to ./src/main.c

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
+#include <libintl.h>	// GetText ...
+#include <locale.h>		// setlocale ...
+#include <stdio.h>		// printf ...
 #include <stdlib.h>
+
+#define _(STRING) gettext(STRING)
 
 int main( int argc, char ** argv)
 {
+	setlocale(LC_ALL,"");
+
+	printf(_("Hello World!\n"));
+
 	return EXIT_SUCCESS;
 }
 
